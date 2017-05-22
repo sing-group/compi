dc3e911be62942180da49193f796bf492cb5475a
lipido <dgpena@uvigo.es>
2017-05-22 20:07:58 +0200

    Adds current dir computation
    In order to allow running compi from a different directory,
    the current dir is calculated to reference the main jar as an
    absolute path.

--- src/main/dist-resources/compi.sh
+++ src/main/dist-resources/compi.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -jar ${project.artifactId}-${project.version}-jar-with-dependencies.jar $@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+java -jar "$DIR/${project.artifactId}-${project.version}-jar-with-dependencies.jar" $@
